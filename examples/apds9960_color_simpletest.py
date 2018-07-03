ab802b2f266abd724076504c9c2921a3cd528cce
Limor "Ladyada" Fried <limor@ladyada.net>
2018-07-03 16:33:12 -0400

    Update apds9960_color_simpletest.py
    use a universally known pin

--- examples/apds9960_color_simpletest.py
+++ examples/apds9960_color_simpletest.py
@@ -6,7 +6,7 @@ from adafruit_apds9960.apds9960 import APDS9960
 from adafruit_apds9960 import colorutility
 
 i2c = busio.I2C(board.SCL, board.SDA)
-int_pin = digitalio.DigitalInOut(board.A2)
+int_pin = digitalio.DigitalInOut(board.D5)
 apds = APDS9960(i2c)
 apds.enable_color = True
 
