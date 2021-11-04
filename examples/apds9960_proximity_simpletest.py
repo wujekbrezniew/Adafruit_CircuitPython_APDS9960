98749fb8f691a563bf4bcc83ef37642888bc6011
Erik Hess <me@erikhess.com>
2021-11-04 14:09:10 -0500

    Removed unused interrupt_pin arg (docs and examples)

--- examples/apds9960_proximity_simpletest.py
+++ examples/apds9960_proximity_simpletest.py
@@ -7,7 +7,7 @@ from adafruit_apds9960.apds9960 import APDS9960
 
 i2c = board.I2C()
 int_pin = digitalio.DigitalInOut(board.D5)
-apds = APDS9960(i2c, interrupt_pin=int_pin)
+apds = APDS9960(i2c)
 
 apds.enable_proximity = True
 apds.proximity_interrupt_threshold = (0, 175)
